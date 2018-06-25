9ab87d0bb348ecf593fbe9f6ba107bcb4577a078
1226151667 <1226151667@qq.com>
2018-06-25 17:24:00 +0800

    基本齐全

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,6 +4,25 @@ use think\Controller;
 
 class Index extends Controller{
     public function index(){
-        return $this->fetch('index');
+        return $this->fetch('index',[
+        	'is_more' => true
+        ]);
+    }
+    public function news(){
+    	if($id = request()->param('id')){
+    		return $this->fetch('new_detail');
+    	}else{
+    		return $this->fetch('new',[
+	        	'is_more' => false
+	        ]);
+    	}
+    }
+    public function contact(){
+    	return $this->fetch('contact');
+    }
+    public function about(){
+    	return $this->fetch('about',[
+        	'is_more' => false
+        ]);
     }
 }
